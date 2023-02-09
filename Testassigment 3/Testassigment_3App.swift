b44646b81725f15ce8b4a63ce66fa2008d1f39e3
TylerFord <tford5@hawk.iit.edu>
2023-02-09 12:51:07 -0600

    Initial Commit

---
+++ Testassigment 3/Testassigment_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Testassigment_3App.swift
+//  Testassigment 3
+//
+//  Created by IIT phys 440 on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Testassigment_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
